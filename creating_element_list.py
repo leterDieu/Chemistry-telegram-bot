ef42aef7a6a3139e82725542fbfb5563c43d83a2
Nikiforov Alexander <99505176+leterDieu@users.noreply.github.com>
2023-11-06 13:40:27 +0300

    changed format of element_list once again

--- creating_element_list.py
+++ creating_element_list.py
@@ -3,7 +3,7 @@ import json
 
 data = {}
 for e in list(pt.elements)[1:]:
-    data[e.symbol.lower()] = {'name': e.name, 'ions': e.ions, 'mass': e.mass, 'number': e.number,
+    data[e.symbol] = {'name': e.name, 'ions': e.ions, 'mass': e.mass, 'number': e.number,
                               'charge': e.charge, 'isotopes': e.isotopes}
 
 with open('elements_list.json', 'w', encoding='utf-8') as w_file:
